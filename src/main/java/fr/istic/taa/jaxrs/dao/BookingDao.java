79aae0667d94ca6f16ebecaacd70dd85a68b0ae6
monelcocou <monelcocou@gmail.com>
2026-02-27 20:46:52 +0100

    feat: Integration des methodes particulieres de Driver DAO et DriverDao

--- src/main/java/fr/istic/taa/jaxrs/dao/BookingDao.java
+++ src/main/java/fr/istic/taa/jaxrs/dao/BookingDao.java
@@ -3,9 +3,53 @@ package fr.istic.taa.jaxrs.dao;
 import fr.istic.taa.jaxrs.dao.generic.AbstractJpaDao;
 import fr.istic.taa.jaxrs.domain.Booking;
 
+import java.util.List;
+
 public class BookingDao extends AbstractJpaDao<Long, Booking> {
     public BookingDao() {
         super();
         setClazz(Booking.class);
     }
+
+    //Trouver les Bookings d'un passager
+    public List<Booking> findByPassenger(Long passengerId) {
+        return entityManager
+                .createQuery(
+                        "SELECT b FROM Booking b WHERE b.passenger.id = :passengerId",
+                        Booking.class
+                )
+                .setParameter("passengerId", passengerId)
+                .getResultList();
+    }
+
+    //Trouver les bookings d'un voyage
+    public List<Booking> findByTrip(Long tripId) {
+        return entityManager
+                .createQuery(
+                        "SELECT b FROM Booking b WHERE b.trip.id = :tripId",
+                        Booking.class
+                )
+                .setParameter("tripId", tripId)
+                .getResultList();
+    }
+
+    //Noter un chauffeur
+    public void rateDriver(Long bookingId, int rating) {
+        Booking booking = findOne(bookingId);
+        if (booking != null) {
+            booking.setDriverRating(rating);
+            update(booking);
+        }
+    }
+
+    //Noter un Passenger
+    public void ratePassenger(Long bookingId, int rating) {
+        Booking booking = findOne(bookingId);
+        if (booking != null) {
+            booking.setPassengerRating(rating);
+            update(booking);
+        }
+    }
+
+
 }
--- src/main/java/fr/istic/taa/jaxrs/dao/DriverDao.java
+++ src/main/java/fr/istic/taa/jaxrs/dao/DriverDao.java
@@ -8,4 +8,15 @@ public class DriverDao extends AbstractJpaDao<Long, Driver> {
         super();
         setClazz(Driver.class);
     }
+
+    //Calcul des moyennes des notes d'un chauffeur
+    public Double calculateAverageRating(Long driverId) {
+        return entityManager
+                .createQuery(
+                        "SELECT AVG(b.driverRating) FROM Booking b WHERE b.trip.driver.id = :driverId",
+                        Double.class
+                )
+                .setParameter("driverId", driverId)
+                .getSingleResult();
+    }
 }
